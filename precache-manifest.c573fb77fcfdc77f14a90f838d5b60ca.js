b8d938c4a20c6456af9611cc324a17a430a1d68b
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-06 15:04:15 -0500

    Updates

---
+++ precache-manifest.c573fb77fcfdc77f14a90f838d5b60ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c99aac53a0aba806308dc41dc0ee58",
+    "url": "/laptop-customizer-MalcolmAndrew/index.html"
+  },
+  {
+    "revision": "1284a40ab8dfd3b869c4",
+    "url": "/laptop-customizer-MalcolmAndrew/static/css/main.705c9c67.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/laptop-customizer-MalcolmAndrew/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "1284a40ab8dfd3b869c4",
+    "url": "/laptop-customizer-MalcolmAndrew/static/js/main.0266e050.chunk.js"
+  },
+  {
+    "revision": "ed3a70a50cf7c6e701e8",
+    "url": "/laptop-customizer-MalcolmAndrew/static/js/runtime~main.8e501c3a.js"
+  }
+]);
